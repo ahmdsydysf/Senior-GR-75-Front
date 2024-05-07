07d7bf622b5ac1fd1997dfb5e4fc4f96e77294c0
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2024-05-07 22:41:21 +0300

    update

--- js/main.js
+++ js/main.js
@@ -53,17 +53,99 @@
 // var myH1 = `<div style="color:red " class='col p-5' ><h1>${name}</h1></div>`;
 // document.write(myH1);
 
-makeAnAlertOnDom();
-myfn();
+// makeAnAlertOnDom();
+// myfn();
 
-function makeAnAlertOnDom() {
-  alert("are you sure !!!!");
-}
+// function makeAnAlertOnDom() {
+//   alert("are you sure !!!!");
+// }
 
-console.log(nameIs);
-var nameIs;
-nameIs = "mahmoud";
-console.log(nameIs);
-var myfn = function () {
-  console.log("i am a variable function");
-};
+// console.log(nameIs);
+// var nameIs;
+// nameIs = "mahmoud";
+// console.log(nameIs);
+// var myfn = function () {
+//   console.log("i am a variable function");
+// };
+
+// function sum() {
+//   var x = 10;
+//   var y = 20;
+//   var sum = x + y;
+//   alert(sum);
+// }
+// sum();
+// function sum(x = 0, y = 0) {
+//   alert(x + y);
+// }
+// sum();
+// parameter  &  arguments
+
+// function printYourName(username , password) {
+//   // alert("your name is : " + username + "and my password is : " + password);
+//   alert(`my name is ${username} and pw ${password}`);
+// }
+// printYourName("ahmed sayed", 123);
+// function test() {
+//   var x = 1;
+//   var y = 2;
+//   var r = x + y;
+//   return r;
+// }
+// // console.log(test());
+
+// var myH1 = document.getElementById("myH");
+// console.log(myH1);
+// myH1.innerText = "red";
+// console.log(myH1);
+
+//const // can't be redeclear or reasign
+
+// const myName = "amed";
+// const Tax = 0.14
+// console.log(myName);
+// var myAge;
+// myAge = 20;
+// console.log(myAge);
+// myAge = 30;
+// var myFileName;
+// myFileName = "index.html";
+
+// var x = "ahmed";
+// console.log(x);
+// console.log(typeof x);
+
+// var y = parseInt("y8u0u");
+// var y1 = Number("80u"); // NAN
+var y2 = 66;
+// console.log(y2);
+// y2.toString();
+// console.log(y2);
+
+// console.log(typeof y2);
+// ahmed, mariam, noran , ziad;
+
+// Array = variable multi values
+// var names = [
+//   "ahmed",
+//   "mariam",
+//   "Ziad",
+//   "nouran",
+//   null,
+//   50,
+//   undefined,
+//   [1, 2, "Sayed"],
+//   y2,
+// ];
+
+// console.log(names);
+// console.log(names[3]);
+// var nouranName = names[3];
+
+var x = 10;
+var y = "10";
+
+// x -= y;// x = x+y
+// x == y // boolean false
+// x === y
+console.log(x !== y); // 11
