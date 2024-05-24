3e5cac8619667963f3f28212d6c9b943e3947e79
ahmdsydysf <a.sayed.xc@gmail.com>
2024-05-24 19:09:37 +0300

    update

--- js/main.js
+++ js/main.js
@@ -21,3 +21,55 @@ function showContent() {
   var content = document.getElementById("content");
   content.innerHTML = txt.value;
 }
+// < > <=  >=  != == === !==    comparisons
+// logical operators | &  ||  &&
+// laptop || book
+// laptop && book
+// var x = 65;
+
+// if (x == 10 || x == 5) {
+//   console.log("ahmed");
+// }
+
+// if (x !== "ahmed") {
+//   console.log("not ahmed");
+// } else {
+//   console.log("ahmmmmmmmmmmed");
+// }
+
+// if (x >= 90 && x < 100) {
+//   console.log("A+");
+// } else if (x < 90 && x > 80) {
+//   console.log("B");
+// } else if (x <= 80 && x > 50) {
+//   console.log("c");
+// } else {
+//   console.log("F");
+// }
+
+// 8  == '8'  true
+// 8  === '8'  false
+
+// console.log(x);
+// var arr = [44, 5, 36, 8];
+// var r = Math.min(...arr);
+// console.log(r);
+// var r = Math.max(44, 5, 36, 8);
+// var r = Math.round(15.44);
+// var r = Math.ceil(15.44);
+// var r = Math.floor(15.99);
+// var r = Math.trunc(15.99);
+// var r = Math.random() * 100; //  random float number between 0  =>  100
+
+// var x = Math.round(r);
+// console.log(x);
+
+var x = "      my name        ";
+
+// r = x.trim().toUpperCase();
+// r = x.concat(" is Ahmed");
+
+// var myname = x.replace("my", "our");
+var r = x.indexOf()
+console.log(myname);
+console.log(x);
