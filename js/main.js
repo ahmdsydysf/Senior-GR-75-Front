b9abed2e85b7813f7a9045e1b899b793bc3b8a80
ahmdsydysf <a.sayed.xc@gmail.com>
2024-05-31 17:10:08 +0300

    update

--- js/main.js
+++ js/main.js
@@ -69,14 +69,72 @@ function showContent() {
 
 // var myname = x.replace("my", "our");
 // index0  length1
-var x = "my name is ahmed";
+// var x = "my name is ahmed";
 
-var r = x.charAt(60);
+// var r = x.charAt(60);
 
-var r1 = x.indexOf("ahmed");
-var r = x.substring(r1);
+// var r1 = x.indexOf("ahmed");
+// var r = x.substring(r1);
 
-// var r = x.length;
+// // var r = x.length;
 
-// console.log(myname);
-console.log(r);
+// // console.log(myname);
+// console.log(r);
+
+// var Myname = "ahmed @ gmail . com";
+
+// console.log(Myname.slice(-8, -3));
+
+// console.log(Myname.split());
+// console.log(Myname.split(""));
+// console.log(Myname.split(" "));
+
+// console.log(Myname.indexOf("ed")); //3
+// console.log(Myname.indexOf("sayed")); //6
+// console.log(Myname.indexOf("sayed", 8)); // -1 no result is found
+
+// console.log(Myname.indexOf("ed", Myname.indexOf("ed") + 1));
+// console.log(Myname.lastIndexOf("ed"));
+// var le = Myname.length;
+// console.log(le);
+// console.log(Myname.includes(".com", le - 4));
+
+// var myString = "Lorem ipsum dolor sit amet consectetur";
+
+// // console.log(myString.slice(0, 3));
+// // console.log(myString.slice(0, 3));
+
+// var len = myString.length;
+// var myP = Math.round(Math.random() * len); // 0 -> 10   33
+// var myP2 = Math.round(Math.random() * len); // 0 -> 10   33
+
+// console.log(myString);
+// console.log(myP);
+// console.log(myString.slice(myP2, myP));
+
+function gen() {
+  var lorem =
+    "ask8784515faf ascdfs523af4we89 fw4897r523 89%^&*#&*(*)__WQ!1234yhrjkncsdjkgweytrw3 78ry";
+  var myPassword = document.getElementById("usernameInput");
+  var mylength = document.getElementById("passwordInput");
+
+  if (mylength.value >= 6) {
+    var finalPass = ""; // 5sdjso8&
+
+    for (var i = 0; i < mylength.value; i++) {
+      var r = Math.round(Math.random() * lorem.length); // 0 -> 60  20
+      finalPass += lorem.slice(r, r + 1);
+    }
+    myPassword.value = finalPass;
+  } else {
+    myPassword.value = "Plz try again more than 6 char";
+  }
+}
+
+function createElementPost() {
+  var newElem = document.createElement("div");
+  var myRow = document.querySelector(".container .row");
+  newElem.innerHTML =
+    "this is new post <button> delete </button> <button> Edit </button>";
+  myRow.appendChild(newElem);
+}
